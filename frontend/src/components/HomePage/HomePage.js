41df9c8ee78ae16a62fa538f8cb3f0e38c9c39c8
DanielRibeiroPGMotta <dribeiropgmotta@gmail.com>
2023-05-12 19:13:20 -0300

    fix: text

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -41,10 +41,6 @@ const HomePage = () => {
                   <h3 className="mb-4 text-xl md:text-2xl leading-tight text-coolGray-900 font-bold">
                     Entenda seus gastos
                   </h3>
-                  <p className="text-coolGray-500 font-medium">
-                    Stop wasting time logging expenses and tracking your income
-                    by hand. Use our software to manage it all in one place.
-                  </p>
                 </div>
               </div>
               <div className="flex flex-wrap p-8 text-center md:text-left hover:bg-white rounded-md hover:shadow-xl transition duration-200">
@@ -66,12 +62,9 @@ const HomePage = () => {
                 </div>
                 <div className="w-full md:flex-1 md:pt-3">
                   <h3 className="mb-4 text-xl md:text-2xl leading-tight text-coolGray-900 font-bold">
-                    Visualize Your Income and Expenses Over Time
+                    Visualize receita e despesas
                   </h3>
-                  <p className="text-coolGray-500 font-medium">
-                    See where your money goes each month and how it changes over
-                    time with this easy-to-use income and expenses tracker.
-                  </p>
+    
                 </div>
               </div>
               <div className="flex flex-wrap p-8 text-center md:text-left hover:bg-white rounded-md hover:shadow-xl transition duration-200">
@@ -93,12 +86,8 @@ const HomePage = () => {
                 </div>
                 <div className="w-full md:flex-1 md:pt-3">
                   <h3 className="mb-4 text-xl md:text-2xl leading-tight text-coolGray-900 font-bold">
-                    Stay on Top of Your Spending
+                    Fique atualizado
                   </h3>
-                  <p className="text-coolGray-500 font-medium">
-                    Take back control of your finances with our easy-to-use
-                    budgeting tool. Get started for free.
-                  </p>
                 </div>
               </div>
             </div>
@@ -114,35 +103,6 @@ const HomePage = () => {
         </div>
       </section>
       {/* Call to Action */}
-      <section className="relative py-24 md:pb-32 bg-white overflow-hidden">
-        <div className="relative container px-4 mx-auto">
-          <div className="xl:max-w-4xl mb-18 md:mb-16 mx-auto text-center">
-            <span className="inline-block py-px px-2 mb-4 text-xs leading-5 text-green-500 bg-green-100 font-medium uppercase rounded-full shadow-sm">
-              Get Started
-            </span>
-            <h1 className="mb-4 text-3xl md:text-4xl leading-tight font-heading font-bold">
-              Save Time and Money with the Cash Flow Tracker
-            </h1>
-            <p className="mb-6 text-lg md:text-xl text-coolGray-500 font-heading">
-              Welcome to Louie's Cash Flow Application Demo, this is the first
-              web application I have made. I hope you enjoy it!
-            </p>
-            <a
-              className="inline-flex items-center justify-center px-7 py-3 h-14 w-full md:w-auto text-lg leading-7 text-green-50 bg-green-500 hover:bg-green-600 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 border border-transparent rounded-md shadow-sm"
-              href="#"
-            >
-              Get Started
-            </a>
-          </div>
-          <div className="relative max-w-4xl mx-auto">
-            <img
-              className="relative"
-              src="https://cdn.pixabay.com/photo/2015/01/28/22/20/bookkeeping-615384_1280.jpg"
-              alt="landing"
-            />
-          </div>
-        </div>
-      </section>
     </>
   );
 };
